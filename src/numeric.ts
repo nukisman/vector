66e81c627fc0849f5cfac87d03ec02678b007969
nukisman <nukisman@gmail.com>
2019-11-22 23:30:41 +0300

    REF: Vector: type aliases

--- src/numeric.ts
+++ src/numeric.ts
@@ -19,14 +19,14 @@ export const showPoint = (v: number[]): string =>
 export const isZero = (v: number[]): boolean =>
   v.reduce((acc: boolean, n) => acc && n === 0, true);
 
-export const zeroV2: V2<number> = [0, 0];
+export const zeroV2: Point2 = [0, 0];
 export const zeroM2: M2<number> = [zeroV2, zeroV2];
 export const identity2: M2<number> = [
   [1, 0],
   [0, 1]
 ];
 
-export const zeroV3: V3<number> = [0, 0, 0];
+export const zeroV3: Point3 = [0, 0, 0];
 export const zeroM3: M3<number> = [zeroV3, zeroV3, zeroV3];
 export const identity3: M3<number> = [
   [1, 0, 0],
@@ -34,29 +34,17 @@ export const identity3: M3<number> = [
   [0, 0, 1]
 ];
 
-export const add = (a: V2<number>, b: V2<number>): V2<number> => [
-  a[0] + b[0],
-  a[1] + b[1]
-];
-export const sub = (a: V2<number>, b: V2<number>): V2<number> => [
-  a[0] - b[0],
-  a[1] - b[1]
-];
-export const mult = (k: number, v: V2<number>): V2<number> => [
-  v[0] * k,
-  v[1] * k
-];
-export const neg = (v: V2<number>): V2<number> => mult(-1, v);
-export const divide = (k: number, v: V2<number>): V2<number> => [
-  k / v[0],
-  k / v[1]
-];
+export const add = (a: Point2, b: Point2): Point2 => [a[0] + b[0], a[1] + b[1]];
+export const sub = (a: Point2, b: Point2): Point2 => [a[0] - b[0], a[1] - b[1]];
+export const mult = (k: number, v: Point2): Point2 => [v[0] * k, v[1] * k];
+export const neg = (v: Point2): Point2 => mult(-1, v);
+export const divide = (k: number, v: Point2): Point2 => [k / v[0], k / v[1]];
 
 /** Скалярное произведение 2D-векторов */
-export const dotProduct2 = (a: V2<number>, b: V2<number>): number =>
+export const dotProduct2 = (a: Point2, b: Point2): number =>
   a[0] * b[0] + a[1] * b[1];
 /** Скалярное произведение 3D-векторов */
-export const dotProduct3 = (a: V3<number>, b: V3<number>): number =>
+export const dotProduct3 = (a: Point3, b: Point3): number =>
   a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 
 export const product3 = (a: M3<number>, b: M3<number>): M3<number> => {
@@ -65,10 +53,7 @@ export const product3 = (a: M3<number>, b: M3<number>): M3<number> => {
 };
 
 //todo: productLine(line, m)
-export const productColumn3 = (
-  m: M3<number>,
-  column: V3<number>
-): V3<number> => [
+export const productColumn3 = (m: M3<number>, column: Point3): Point3 => [
   dotProduct3(m[0], column),
   dotProduct3(m[1], column),
   dotProduct3(m[2], column)
